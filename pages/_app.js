609d55599be5e9bdc801f11c42d0e733f9e77ee1
Annie <anniehartmann97@gmail.com>
2023-01-21 13:07:12 +0300

    Fix bug with styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,17 @@ import Theme from "@/styles/Theme";
 import GlobalStyles from "@/styles/GlobalStyles";
 import Nav from "@/components/Nav";
 import { Inter } from "@next/font/google";
-import GitBookButton from "@/components/GitBookButton";
 const inter = Inter({ subsets: ["latin"] });
+import GitBookButton from "@/components/GitBookButton";
 import { BallTriangle } from "react-loader-spinner";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 
 export default function App({ Component, pageProps }) {
   const [isLoad, setIsLoad] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      // setIsLoad(false);
+      setIsLoad(false);
     }, 1000);
   }, []);
 
@@ -23,22 +22,20 @@ export default function App({ Component, pageProps }) {
       <Theme>
         <main className={inter.className}>
           {isLoad ? (
-            <Loader>
-              <BallTriangle
-                height={100}
-                width={100}
-                radius={5}
-                color="#93D400"
-                ariaLabel="ball-triangle-loading"
-                wrapperClass={"loader"}
-                wrapperStyle={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100vh",
-                }}
-                visible={true}
-              />
-            </Loader>
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#93D400"
+              ariaLabel="ball-triangle-loading"
+              wrapperClass={"loader"}
+              wrapperStyle={{
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+              }}
+              visible={true}
+            />
           ) : (
             <>
               <Nav />
@@ -51,11 +48,3 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
-
-const Loader = styled.div`
-  /* display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh; */
-`;
